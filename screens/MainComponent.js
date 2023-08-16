6f942da98ed306758b90a6cf47aebb04fbc25fb0
veganpowerlifter <katya@totalbodylab.com>
2023-08-16 18:53:35 -0400

    navigation updated home screen

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,22 +1,37 @@
-// import { useState } from 'react';
-// import { CAMPSITES } from '../shared/campsites';
-import CampsiteInfoScreen from './CampsiteInfoScreen';
-import DirectoryScreen from './DirectoryScreen';
 import { Platform, View } from 'react-native';
 import Constants from 'expo-constants';
+import CampsiteInfoScreen from './CampsiteInfoScreen';
+import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+    headerTintColor: '#fff',
+    headerStyle: { backgroundColor: '#5637DD' }
+};
+
+const HomeNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{ title: 'Home' }}
+            />
+        </Stack.Navigator>
+    );
+};
 
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
     return (
         <Stack.Navigator
             initialRouteName='Directory'
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#5637DD'
-                },
-                headerTintColor: '#fff'
-            }}
+            screenOptions={screenOptions}
         >
             <Stack.Screen
                 name='Directory'
@@ -35,19 +50,33 @@ const DirectoryNavigator = () => {
 };
 
 const Main = () => {
- //   const [campsites, setCampsites] = useState(CAMPSITES);
-  //      const [selectedCampsiteId, setSelectedCampsiteId] = useState();
-  return (
-    <View
-        style={{
-            flex: 1,
-            paddingTop:
-                Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
-        }}
-    >
-        <DirectoryNavigator />
-    </View>
-);
+    //   const [campsites, setCampsites] = useState(CAMPSITES); why did we delete these?
+ //     const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+    return (
+        <View
+            style={{
+                flex: 1,
+                paddingTop:
+                    Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            }}
+        >
+            <Drawer.Navigator
+                initialRouteName='Home'
+                drawerStyle={{ backgroundColor: '#CEC8FF' }}
+            >
+                <Drawer.Screen
+                    name='Home'
+                    component={HomeNavigator}
+                    options={{ title: 'Home' }}
+                />
+                <Drawer.Screen
+                    name='Directory'
+                    component={DirectoryNavigator}
+                    options={{ title: 'Directory' }}
+                />
+            </Drawer.Navigator>
+        </View>
+    );
 };
 
 export default Main;
